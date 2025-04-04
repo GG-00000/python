c20485163666f70d7b7ac85ad1554c2ab3610cfd
Gutierrez <ggutierrez48@cps.k12.il.us>
2025-04-04 12:02:09 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,93 @@
+print("I like pizza")
+print("Its really good!")
+first_name = "bro"
+print(first_name)
+food = "pizza"
+print(f"hello {first_name}")
+print(f"You like {food}")
+enail = "Bro123@fake.com"
+# f string allows you to insert a variabel into a string
+# strings are text 
+name = "John"
+name2 = "Lucy"
+# quatation marks
+num1 = "10"
+num2 = 20
+print(int(num1) + num2)
+# a plus sign between two bariables its called  concatenation
+print(name +"and"+ name2 + "have" + num1 + str(num2) + "apples")
+
+# there are different types of variales
+# integers are whole number 
+# floats are decimal 
+# bootleans are true or false 
+# lists are  collections of values
+#dictionaries are collections of key-valie pair
+print(f"{name} and {name2} have {num1} apples")
+
+
+dollars = 10.99
+print(f"{name} has {dollars} dollars")
+is_student = True
+print(f"{name} is a student: {is_student}")
+
+price1= 1.00
+price2= 5.00
+price3= 10.00
+price4= 20.00
+
+print(f" {price1} is cheap {price2} is less cheap {price3} is somewhat expensive {price4} is really expensive")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
